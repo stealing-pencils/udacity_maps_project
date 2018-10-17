f4863a9861f698dfdf16b1d56b622a3763b24a41
Katherine Riley <unoscarto@hotmail.com>
2018-10-17 11:31:06 -0700

    added basic html structure

--- nz_coffee_maps_project/src/AppMap.js
+++ nz_coffee_maps_project/src/AppMap.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react';
 class AppMap extends Component {
   render() {
     return (
-
-
+      <div className= 'map-body'>
+        <div className= 'third-party-api-reference'>
+        </div>
+      </div>
     )
   }
 }
