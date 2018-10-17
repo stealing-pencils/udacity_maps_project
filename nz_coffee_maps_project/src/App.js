8b0177a816bee9cbb5de73e7923ce2cfa35059b0
Katherine Riley <unoscarto@hotmail.com>
2018-10-17 11:01:53 -0700

    added to do list

--- nz_coffee_maps_project/src/App.js
+++ nz_coffee_maps_project/src/App.js
@@ -6,8 +6,19 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-        
         </header>
+
+        {/* TODO Add header*/}
+
+        { /* TODO add SearchBar component}
+
+        {/* TODO add Map component */}
+
+        {/* TODO add results list component - child compnent of searchbar? */}
+
+
+        { /* TODO Add footer*/}
+
       </div>
     );
   }
