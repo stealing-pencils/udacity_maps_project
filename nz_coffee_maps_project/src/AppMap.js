a89b891d61d62e86a161de94d97cd6f81769d1db
Katherine Riley <unoscarto@hotmail.com>
2018-10-17 15:37:46 -0700

    created basic responsive css structure

--- nz_coffee_maps_project/src/AppMap.js
+++ nz_coffee_maps_project/src/AppMap.js
@@ -4,7 +4,6 @@ class AppMap extends Component {
   render() {
     return (
       <div className= 'map-body'>
-      map body
         <div className= 'third-party-api-reference'>
         </div>
       </div>
