a5bba509e53fd51bdfc6290bd38fb2fdd5d41605
Katherine Riley <unoscarto@hotmail.com>
2018-10-17 11:43:09 -0700

    wiped the started code

--- nz_coffee_maps_project/src/App.js
+++ nz_coffee_maps_project/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-        
+        Hey there
         </header>
 
         <SearchBar />
