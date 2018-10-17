71060f7406ffb33e71b41522c4c190e6e5fb7fb7
Katherine Riley <unoscarto@hotmail.com>
2018-10-17 11:29:28 -0700

    built basic html structure

--- nz_coffee_maps_project/src/SearchBar.js
+++ nz_coffee_maps_project/src/SearchBar.js
@@ -4,8 +4,12 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
   render() {
     return (
-
-
+      <div className = 'search-bar-body'>
+        <div className = 'text-input'>
+        </div>
+        <button className = 'search-button'>
+        </button>
+      </div>
     )
   }
 }
