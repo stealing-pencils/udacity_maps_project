1d4946788ea6256822576eaf081fca416989921a
Katherine Riley <unoscarto@hotmail.com>
2018-10-17 15:57:23 -0700

    using forrest walker walkthrough added map

--- nz_coffee_maps_project/src/AppMap.js
+++ nz_coffee_maps_project/src/AppMap.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+
+const MyMapComponent = withScriptjs(withGoogleMap((props) =>
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+  >
+    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+  </GoogleMap>
+))
 
 class AppMap extends Component {
   render() {
     return (
       <div className= 'map-body'>
+
+        <MyMapComponent
+          isMarkerShown
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyBJ39aLUnpQEi-Ewf6EIIKguFlX-z_SNbw"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `570px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
         <div className= 'third-party-api-reference'>
         </div>
       </div>
