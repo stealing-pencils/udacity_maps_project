2dcb5db86f1b3841d75f60b6b959c1e436698d43
Katherine Riley <unoscarto@hotmail.com>
2018-10-18 14:49:28 -0700

    using Forrest Walker walkthrough created file to fetch foursquare api data

---
+++ nz_coffee_maps_project/src/API/index.js
@@ -0,0 +1,48 @@
+class Helper {
+  static baseURL() {
+    return "https://apifoursquare.com/v2"
+  }
+  static auth(){
+    const keys = {
+      client_id: "OVXN3KG3ITFHVC2XKVARXSTXTSHRLL0OVRIUQCQE53WMPOUO",
+      client_secret: "BG0GE00C5PVFMNQIODGMNXSC0A0JB4WKHVIII11DR5C3F2L0",
+      v: "20181018"
+    };
+    return Object.keys(keys).map(key => `${key} = ${keys[key]}`)
+    .join("&");
+  }
+  static urlBuilder(urlPrams){
+    if(!urlPrams){
+      return ""
+    }
+    return Object.jeys(urlPrams).map(key => `${key}=${urlPrams[key]}`)
+    .join("&");
+  }
+  static headers() {
+    return {
+      Accept: "application/json"
+    };
+  }
+  static simpleFetch(endPoint, method, urlPrams){
+    let requestData = {
+      method,
+      headers: Helper.headers()
+    };
+    return fetch(
+      `${Helper.baseURL()}${endPoint}?{Helper.auth()}&{Helper.urlBuilder(urlPrams)}`,
+      requestData
+    ).then(res => res.json());
+  }
+}
+
+export default class fourSquareAPI {
+  static search(urlPrams){
+    return Helper.simpleFetch("/venues/search", "GET", urlPrams);
+  }
+  static getVenueDetails(VENUE_ID){
+    return Helper.simpleFetch(`/venues/${VENUE_ID}`, "GET");
+  }
+  static getVenuePhotos(VENUE_ID) {
+    return Helper.simpleFetch(`/venues/${VENUE_ID}/photos`, "GET");
+  }
+}
