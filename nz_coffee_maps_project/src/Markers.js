797864ddaedbbb2c4e5baaa8d4f24d13be2811ac
Katherine Riley <unoscarto@hotmail.com>
2018-10-22 15:17:56 -0700

    decided to create separate markers component

---
+++ nz_coffee_maps_project/src/Markers.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+
+
+class Markers extends Component {
+  
+
+
+  render() {
+    return (
+      <div className = "map">
+
+
+      </div>
+    )
+  }
+}
+
+export default Markers
