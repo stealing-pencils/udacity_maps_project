3bbb07eeffa3743bc90e290429294efb4f9657a8
Katherine Riley <unoscarto@hotmail.com>
2018-10-22 15:20:37 -0700

    moved foursquare api request to markers component

--- nz_coffee_maps_project/src/AppMap.js
+++ nz_coffee_maps_project/src/AppMap.js
@@ -2,35 +2,16 @@ import React, { Component } from 'react';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 import Markers from './Markers'
 
-var foursquare = require('react-foursquare')({
-clientID: 'OVXN3KG3ITFHVC2XKVARXSTXTSHRLL0OVRIUQCQE53WMPOUO',
-clientSecret: 'TQTIW2FA04GLWPHBWBCK20YFKRNZ0H25PRRCTRANBZWWUTTG'
-});
 
-var params = {
-"near": "Auckland, NZ",
-"query": 'coffee'
-};
 
 
 
 class AppMap extends Component {
 
   state = {
-    markers: [],
     venues: [],
     selectedPlace: []
   }
-
-
-
-  componentDidMount() {
-    foursquare.venues.getVenues(params)
-      .then(res=> {
-        this.setState({ venues: res.response.venues });
-      });
-  }
-
 
 
   render() {
--- nz_coffee_maps_project/src/Markers.js
+++ nz_coffee_maps_project/src/Markers.js
@@ -2,9 +2,28 @@ import React, { Component } from 'react';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 
-class Markers extends Component {
-  
+var foursquare = require('react-foursquare')({
+clientID: 'OVXN3KG3ITFHVC2XKVARXSTXTSHRLL0OVRIUQCQE53WMPOUO',
+clientSecret: 'TQTIW2FA04GLWPHBWBCK20YFKRNZ0H25PRRCTRANBZWWUTTG'
+});
 
+var params = {
+"near": "Auckland, NZ",
+"query": 'coffee'
+};
+
+class Markers extends Component {
+
+  state = {
+    venues: 'placeholder'
+  }
+
+  componentDidMount() {
+    foursquare.venues.getVenues(params)
+      .then(res=> {
+        this.setState({ venues: res.response.venues });
+      });
+  }
 
   render() {
     return (
