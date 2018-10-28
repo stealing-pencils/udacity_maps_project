de4b863042fc7042011cc77863803443432f4091
Katherine Riley <unoscarto@hotmail.com>
2018-10-28 16:12:26 -0700

    added event listener to record the query input

--- nz_coffee_maps_project/src/SearchBar.js
+++ nz_coffee_maps_project/src/SearchBar.js
@@ -2,15 +2,32 @@ import React, { Component } from 'react';
 
 
 class SearchBar extends Component {
+
+  state = {
+    query: '',
+  }
+
+  updateQuery = (query ) => {
+    this.setState({ query : query },_=> {
+    })
+  }
+  
+
   render() {
+    console.log(this.state.query)
     return (
       <div className = 'search-bar-body'>
         <div className = 'search-bar-title'>
           <h2>find what you fancy</h2>
         </div>
-
         <div className = 'search-field'>
-          <input id="search-location-text" type="text" placeholder="Enter your favorite area!"/>
+          <input
+            id="search-location-text"
+            type="text"
+            placeholder="Enter your favorite area!"
+            value={this.state.query}
+            onChange={(event) => this.updateQuery(event.target.value)}
+            />
           <input id="search-location-button" type="button" value="Zoom"/>
         </div>
       </div>
