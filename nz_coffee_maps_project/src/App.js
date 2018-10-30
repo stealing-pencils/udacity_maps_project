d2deaccb97ce8e0da03fcd22ff347b600c6a7c1d
Katherine Riley <unoscarto@hotmail.com>
2018-10-30 14:56:45 -0700

    moved foursquare request and connected states to main app file making necessary changes on the AppMap component as a result

--- nz_coffee_maps_project/src/App.js
+++ nz_coffee_maps_project/src/App.js
@@ -4,10 +4,45 @@ import AppMap from './AppMap.js'
 import './App.css';
 
 
+var foursquare = require('react-foursquare')({
+clientID: 'OVXN3KG3ITFHVC2XKVARXSTXTSHRLL0OVRIUQCQE53WMPOUO',
+clientSecret: 'TQTIW2FA04GLWPHBWBCK20YFKRNZ0H25PRRCTRANBZWWUTTG'
+});
+
+var params = {
+"near": "Auckland, NZ",
+"query": `$"{this.props.query}"`
+};
+
+
 class App extends Component {
 
   state = {
     query: '',
+    venues: [],
+    center: [],
+    markers: []
+  }
+
+  componentDidMount() {
+    foursquare.venues.getVenues(params)
+      .then(res=> {
+        const {venues} = res.response
+        const {center} = res.response.geocode.feature.geometry
+        const markers = venues.map(venue => {
+          return {
+            name: venue.name,
+            lat: venue.location.lat,
+            lng: venue.location.lng,
+            isOpen: false,
+            isVisible: true,
+            id: venue.id,
+            address: venue.location.address,
+            formatted_address: venue.location.formattedAddress
+          }
+        })
+        this.setState({venues, center, markers})
+      });
   }
 
   // saves user search query
@@ -43,6 +78,9 @@ class App extends Component {
         <div className= 'map-body'>
           <AppMap
           query = {this.state.query}
+          markers = {this.state.markers}
+          venues = {this.state.venues}
+          center = {this.state.center}
           />
         </div>
 
