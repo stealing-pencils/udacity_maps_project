1d42ef200a7c58539e6224e8fe7f4fc97ce43d70
Katherine Riley <unoscarto@hotmail.com>
2018-10-30 13:41:13 -0700

    refactored code so the search bar code now appears on the main app.js which will enable me to continue using CSS grid

--- nz_coffee_maps_project/src/App.js
+++ nz_coffee_maps_project/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import SearchBar from './SearchBar.js';
 import ResultsList from './ResultsList.js';
 import AppMap from './AppMap.js'
 import './App.css';
@@ -7,24 +6,51 @@ import './App.css';
 
 class App extends Component {
 
+  state = {
+    query: '',
+  }
+
+  // saves user search query
+  updateQuery = (query ) => {
+    this.setState({ query : query },_=> {
+    })
+  }
+
   render() {
 
     return (
       <div className="App">
 
         <header className="App-header">
-        Eat New Zealand
+          Eat New Zealand
         </header>
+        <div className = "search-bar-body">
+          <div className = "search-bar-title">
+            <h2>find what you fancy</h2>
+          </div>
+          <div className = 'search-field'>
+            <input
+              id="search-location-text"
+              type="text"
+              placeholder="Enter your favorite area!"
+              value={this.state.query}
+              onChange={(event) => this.updateQuery(event.target.value)}
+              />
+            <input id="search-location-button" type="button" value="Zoom"/>
+          </div>
+        </div>
 
-        <SearchBar />
-
-        <ResultsList />
         <div className= 'map-body'>
-          <AppMap />
+          <AppMap
+          query = {this.state.query}
+          />
+        </div>
+
+        <div className = "list-body">
+          <ResultsList />
         </div>
         <footer className="App-footer">
         </footer>
-
       </div>
     );
   }
